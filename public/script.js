2939027cb3422c87f4535f8b7cf6616302ddd736
TimPaisley <tepaisley@gmail.com>
2017-08-17 15:20:05 +1200

    Added css stones and basic code for positioning on board

--- public/script.js
+++ public/script.js
@@ -1,5 +1,19 @@
 $(function () {
 
+  createStone(3, 4, "white");
+  createStone(5, 7, "black");
+  createStone(1, 1, "black");
+
+  function createStone(x, y, colour) {
+    var table = $("#board-table");
+    var stone = $("<div>").addClass("stone").addClass(colour);
+
+    stone.css('top', 6.25 + ((y-2) * 12.5) + "%");
+    stone.css('left', 6.25 + ((x-2) * 12.5) + "%");
+
+    table.append(stone);
+  }
+
   // messages
 
   var socket = io();
